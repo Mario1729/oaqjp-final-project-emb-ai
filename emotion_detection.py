9db0ed0b9011c38abf83de916838c5b911726b02
Mario <Mario1729@github.com>
2025-08-25 08:49:18 -0400

    Added formatting and exception handling to emotion detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,17 @@
-import requests
+"""
+Emotion Detection Module - IBM Watson AI Engine
+"""
+
 import json
+import requests
 
 def emotion_detector(text_to_analyse):
+    """Emotion Detector using IBM Watson Emotion API
+    """
     # Define the URL for the emotion detection API
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    base_url = 'https://sn-watson-emotion.labs.skills.network/v1/'
+    endpoint = 'watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = base_url + endpoint
 
     # Create the payload with the text to be analyzed
     myobj = { "raw_document": { "text": text_to_analyse } }
@@ -11,19 +19,38 @@ def emotion_detector(text_to_analyse):
     # Set the headers with the required model ID for the API
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
+    # Checking for empty input/white spaces
+    if (text_to_analyse is None) or (not text_to_analyse.strip()):
+        return {'input error': 'Please enter valid text, not empty or whitespace'}
+
     # Make a POST request to the API with the payload and headers
-    response = requests.post(url, json=myobj, headers=header)
+    try:
+        response = requests.post(url, json=myobj, headers=header, timeout=20)
+    except requests.exceptions.Timeout:
+        return {'error': 'The request timed out!'}
+    except requests.exceptions.RequestException as e:
+        return {'error': f'An error occurred: {e}'}
 
-    # Parse the response from the API
-    formatted_response = json.loads(response.text)
-
-    # If the response status code is 200, extract the label and score from the response
-    if response.status_code == 200:
-        pass
-    # If the response status code is 500, set label and score to None
-    elif response.status_code == 500:
-        pass
-
+    # If the response status code is 200
+    if 200 <= response.status_code < 300:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        return_response = {
+                            'anger': emotion_scores['anger'],
+                            'disgust': emotion_scores['disgust'],
+                            'fear': emotion_scores['fear'],
+                            'joy': emotion_scores['joy'],
+                            'sadness': emotion_scores['sadness'],
+                            'dominant_emotion': dominant_emotion
+                            }
+    # If the response status code is 500
+    elif 400 <= response.status_code < 500:
+        return_response = {f'input error {response.status_code}': 'Please enter valid text'}
+    elif 500 <= response.status_code < 600:
+        return_response = {f'server error {response.status_code}': 'Please try again'}
+    else:
+        return_response = {f'info {response.status_code}': response.text}
     # Return response
-    return formatted_response
-    
+    return return_response
